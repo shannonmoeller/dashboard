aa759ab1634c469efb89183b375ec16630176fe1
Shannon Moeller <me@shannonmoeller.com>
2014-10-24 11:50:10 -0400

    Moved repos to config file.

--- generate.js
+++ generate.js
@@ -1,86 +1,45 @@
 /* jshint node:true */
 'use strict';
 
-var fs = require('fs');
+var fs = require('fs'),
+	repos = require('./repos'),
+	file = __dirname + '/README.md';
 
 function generate(accounts) {
-    var stream,
-        file = __dirname + '/README.md';
+	var stream;
 
-    // Delete
-    fs.unlinkSync(file);
+	// Delete
+	fs.unlinkSync(file);
 
-    // Create
-    stream = fs.createWriteStream(file);
-    stream.write('# dashboard\n');
+	// Create
+	stream = fs.createWriteStream(file);
+	stream.write('# dashboard\n');
 
-    // Generate
-    accounts.forEach(function (account) {
-        var user = account.user;
+	// Generate
+	accounts.forEach(function (account) {
+		var user = account.user;
 
-        // Header
-        stream.write('\n' + user + ' | version | downloads | status | coverage | dependencies | devDependencies\n');
-        stream.write('---|---|---|---|---|---|---\n');
+		// Header
+		stream.write('\n' + user + ' | version | downloads | status | coverage | dependencies | devDependencies\n');
+		stream.write('---|---|---|---|---|---|---\n');
 
-        // Rows
-        account.repos.forEach(function (repo) {
-            var name = repo.name,
-                npm = repo.npm || name,
-                id = user + '/' + name;
+		// Rows
+		account.repos.forEach(function (repo) {
+			var name = repo.name,
+				npm = repo.npm || name,
+				id = user + '/' + name;
 
-            stream.write(
-                '[' + name + '](http://github.com/' + id + ') ' +
-                '| [![version](http://img.shields.io/npm/v/' + npm + '.svg?style=flat-square)](http://npmjs.org/' + npm + ') ' +
-                '| [![downloads](http://img.shields.io/npm/dm/' + npm + '.svg?style=flat-square)](http://npmjs.org/' + npm + ') ' +
-                '| [![status](http://img.shields.io/travis/' + id + '.svg?style=flat-square)](https://travis-ci.org/' + id + ')' +
-                '| [![coverage](http://img.shields.io/coveralls/' + id + '/master.svg?style=flat-square)](https://coveralls.io/r/' + id + ')' +
-                '| [![dependencies](http://david-dm.org/' + id + '.svg?style=flat-square)](http://david-dm.org/' + id + ') ' +
-                '| [![devDependencies](http://david-dm.org/' + id + '/dev-status.svg?style=flat-square)](http://david-dm.org/' + id + ')\n'
-            );
-        });
-    });
+			stream.write(
+				'[' + name + '](http://github.com/' + id + ') ' +
+				'| [![version](http://img.shields.io/npm/v/' + npm + '.svg?style=flat-square)](http://npmjs.org/' + npm + ') ' +
+				'| [![downloads](http://img.shields.io/npm/dm/' + npm + '.svg?style=flat-square)](http://npmjs.org/' + npm + ') ' +
+				'| [![status](http://img.shields.io/travis/' + id + '.svg?style=flat-square)](https://travis-ci.org/' + id + ')' +
+				'| [![coverage](http://img.shields.io/coveralls/' + id + '/master.svg?style=flat-square)](https://coveralls.io/r/' + id + ')' +
+				'| [![dependencies](http://david-dm.org/' + id + '.svg?style=flat-square)](http://david-dm.org/' + id + ') ' +
+				'| [![devDependencies](http://david-dm.org/' + id + '/dev-status.svg?style=flat-square)](http://david-dm.org/' + id + ')\n'
+			);
+		});
+	});
 }
 
-generate([
-    {
-        user: 'shannonmoeller',
-        repos: [
-            { name: 'coat' },
-            { name: 'connect-spawn' },
-            { name: 'copier.js', npm: 'copier' },
-            { name: 'doxit' },
-            { name: 'func.js', npm: 'func' },
-            { name: 'get-context' },
-            { name: 'gimmie.js', npm: 'gimmie' },
-            { name: 'grunt-force' },
-            { name: 'grunt-hbt' },
-            { name: 'gulp-hb' },
-            { name: 'handlebars-layouts' },
-            { name: 'hunk' },
-            { name: 'mute' },
-            { name: 'nopt-grunt' },
-            { name: 'sire' },
-            { name: 'workit' }
-        ]
-    },
-    {
-        user: 'militiajs',
-        repos: [
-            { name: 'mdom' },
-            { name: 'mjax' },
-            { name: 'mmvc' },
-            { name: 'mtil' }
-        ]
-    },
-    {
-        user: 'togajs',
-        repos: [
-            { name: 'toga' },
-            { name: 'toga-compiler-pulla' },
-            { name: 'toga-compiler-pura' },
-            { name: 'toga-formatter-markdown' },
-            { name: 'toga-parser-js' },
-            { name: 'tunic' }
-        ]
-    }
-]);
+generate(repos);
---
+++ repos.js
@@ -0,0 +1,46 @@
+/* jshint node:true */
+'use strict';
+
+module.exports = [
+	{
+		user: 'shannonmoeller',
+		repos: [
+			{ name: 'coat' },
+			{ name: 'connect-spawn' },
+			{ name: 'copier.js', npm: 'copier' },
+			{ name: 'doxit' },
+			{ name: 'func.js', npm: 'func' },
+			{ name: 'get-context' },
+			{ name: 'gimmie.js', npm: 'gimmie' },
+			{ name: 'grunt-force' },
+			{ name: 'grunt-hbt' },
+			{ name: 'gulp-hb' },
+			{ name: 'handlebars-layouts' },
+			{ name: 'hunk' },
+			{ name: 'mute' },
+			{ name: 'nopt-grunt' },
+			{ name: 'sire' },
+			{ name: 'workit' }
+		]
+	},
+	{
+		user: 'militiajs',
+		repos: [
+			{ name: 'mdom' },
+			{ name: 'mjax' },
+			{ name: 'mmvc' },
+			{ name: 'mtil' }
+		]
+	},
+	{
+		user: 'togajs',
+		repos: [
+			{ name: 'toga' },
+			{ name: 'toga-compiler-pulla' },
+			{ name: 'toga-compiler-pura' },
+			{ name: 'toga-formatter-markdown' },
+			{ name: 'toga-parser-js' },
+			{ name: 'tunic' }
+		]
+	}
+];
