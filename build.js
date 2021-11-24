d43c4939c92941ff04255898bb67fb601a478047
Shannon Moeller <me@shannonmoeller.com>
2021-11-24 18:27:19 +0000

    use official api and rebuild

--- build.js
+++ build.js
@@ -1,70 +1,27 @@
-const Registry = require('npm-registry');
-const npm = new Registry({ registry: 'https://registry.npmjs.org' });
-const fs = require('fs');
+import { writeFile } from 'fs/promises';
+import { dirname, join } from 'path';
+import fetch from 'npm-registry-fetch';
 
-const file = __dirname + '/index.md';
+const { pathname } = new URL(import.meta.url);
+const filename = join(dirname(pathname), 'index.md');
 
-function getUserPackages(user) {
-	return new Promise(function(resolve, reject) {
-		npm.users.list(user, function(err, data) {
-			return err ? reject(err) : resolve(data);
-		});
-	});
+const packages = await fetch.json('/-/user/shannonmoeller/package');
+const packageNames = Object.keys(packages).sort();
+let contents = '---\n---\n\n# Shannon Moeller’s Dashboard\n\n';
+
+for (const packageName of packageNames) {
+	console.log(packageName);
+
+	const meta = await fetch.json(`/${packageName}`);
+	const matches = meta.repository?.url?.match(/github.com\/(.*?)(.git)?$/);
+	const [, id] = matches || [];
+
+	contents +=
+		`[${packageName}](https://github.com/${id}) | ` +
+		`[![version](https://img.shields.io/npm/v/${packageName}.svg?style=flat-square)](https://npmjs.org/package/${packageName}) | ` +
+		`[![downloads](https://img.shields.io/npm/dm/${packageName}.svg?style=flat-square)](https://npmjs.org/package/${packageName}) | ` +
+		`[![deps](https://img.shields.io/david/${id}.svg?style=flat-square)](https://david-dm.org/${id}) | ` +
+		`[![devDeps](https://img.shields.io/david/dev/${id}.svg?style=flat-square)](https://david-dm.org/${id})\n`;
 }
 
-function getPackageInfo(pkg) {
-	return new Promise(function(resolve, reject) {
-		npm.packages.get(pkg.name, function(err, data) {
-			return err ? reject(err) : resolve(data);
-		});
-	});
-}
-
-function getPackagesInfo(pkgs) {
-	return Promise.all(pkgs.map(getPackageInfo));
-}
-
-function appendRow(stream, pkg) {
-	pkg = pkg.pop();
-
-	const repo = pkg.repository.url.split('/').slice(-2);
-	const repoUser = repo[0];
-	const repoName = repo[1];
-	const npmName = pkg.name;
-	const id = repo.join('/');
-
-	stream.write(
-		[
-			`[${npmName}](https://github.com/${id})`,
-			`[![version](https://img.shields.io/npm/v/${npmName}.svg?style=flat-square)](https://npmjs.org/package/${npmName})`,
-			`[![downloads](https://img.shields.io/npm/dm/${npmName}.svg?style=flat-square)](https://npmjs.org/package/${npmName})`,
-			`[![status](https://img.shields.io/travis/${id}.svg?style=flat-square)](https://travis-ci.org/${id})`,
-			`[![coverage](https://img.shields.io/coveralls/${id}/master.svg?style=flat-square)](https://coveralls.io/r/${id})`,
-			`[![climate](https://img.shields.io/codeclimate/github/${id}.svg?style=flat-square)](https://codeclimate.com/github/${id})`,
-			`[![deps](https://img.shields.io/david/${id}.svg?style=flat-square)](https://david-dm.org/${id})`,
-			`[![devDeps](https://img.shields.io/david/dev/${id}.svg?style=flat-square)](https://david-dm.org/${id})\n`
-		].join(' | ')
-	);
-}
-
-function buildTable(users) {
-	let stream;
-
-	// Delete
-	fs.unlinkSync(file);
-
-	// Create
-	stream = fs.createWriteStream(file);
-	stream.write('---\n---\n\n# Shannon Moeller’s Dashboard\n\n');
-
-	// Write
-	users.forEach(function(pkgs) {
-		pkgs.forEach(appendRow.bind(null, stream));
-	});
-}
-
-Promise.all([
-	getUserPackages('shannonmoeller').then(getPackagesInfo),
-	getUserPackages('toga').then(getPackagesInfo),
-	getUserPackages('ygor').then(getPackagesInfo)
-]).then(buildTable);
+await writeFile(filename, contents);
