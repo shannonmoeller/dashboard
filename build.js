4364f694520b02eb1d975371c5d003d2b08253ca
Shannon Moeller <me@shannonmoeller.com>
2021-11-24 18:31:06 +0000

    drop david

--- build.js
+++ build.js
@@ -10,18 +10,10 @@ const packageNames = Object.keys(packages).sort();
 let contents = '---\n---\n\n# Shannon Moeller’s Dashboard\n\n';
 
 for (const packageName of packageNames) {
-	console.log(packageName);
-
-	const meta = await fetch.json(`/${packageName}`);
-	const matches = meta.repository?.url?.match(/github.com\/(.*?)(.git)?$/);
-	const [, id] = matches || [];
-
 	contents +=
-		`[${packageName}](https://github.com/${id}) | ` +
+		`[${packageName}](https://npm.im/${packageName}) | ` +
 		`[![version](https://img.shields.io/npm/v/${packageName}.svg?style=flat-square)](https://npmjs.org/package/${packageName}) | ` +
-		`[![downloads](https://img.shields.io/npm/dm/${packageName}.svg?style=flat-square)](https://npmjs.org/package/${packageName}) | ` +
-		`[![deps](https://img.shields.io/david/${id}.svg?style=flat-square)](https://david-dm.org/${id}) | ` +
-		`[![devDeps](https://img.shields.io/david/dev/${id}.svg?style=flat-square)](https://david-dm.org/${id})\n`;
+		`[![downloads](https://img.shields.io/npm/dm/${packageName}.svg?style=flat-square)](https://npmjs.org/package/${packageName})\n`;
 }
 
 await writeFile(filename, contents);
